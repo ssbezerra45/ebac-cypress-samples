4d2a4b65f6385afa0cacad69a07da88f19626e09
sergiobezerra <ssbezerra45@gmail.com>
2026-06-07 17:04:08 -0300

    Publicação no GH Pages

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,14 @@
 const { defineConfig } = require("cypress");
+const { allureCypress } = require ("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+       allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
+    video: true
   },
 });
